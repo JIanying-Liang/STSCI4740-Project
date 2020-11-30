7296f19b733876b04baac6331e0ed27256c63ac7
建颖 梁 <UnaLiang@jianyings-MacBook-Pro.local>
2020-11-30 19:07:53 +0800

    multinomial

--- ordinal logistic regression.R
+++ ordinal logistic regression.R
@@ -3,13 +3,11 @@ summary(cancer_data_new)
 #cancer_data_new[which(cancer_data_new$Level=='Low'),24]<-0
 #cancer_data_new[which(cancer_data_new$Level=='Medium'),24]<-0.5
 #cancer_data_new[which(cancer_data_new$Level=='High'),24]<-1
-cancer_data_new$Age<-as.factor(cancer_data_new$Age)
-cancer_data_new$Gender<-as.factor(cancer_data_new$Gender)
-cancer_data_new$`Air Pollution`<-as.factor(cancer_data_new$`Air Pollution`)
+cancer_data_new_2<-cancer_data_new[,2:24]
+cancer_data_new_2<-apply(cancer_data_new_2,2,factor)
+cancer_data_new<-cbind(cancer_data[,2],cancer_data_new_2)
 
-for (i in 1:24){
-  cancer_data_new[,i]<-as.factor(cancer_data_new[,i])
-}
+#Ordinal logistic regression
 set.seed(1)
 training_index<-sample(1:nrow(cancer_data_new),0.8*nrow(cancer_data_new))
 #glm.fit<-glm(Level~.,data = cancer_data_new[training_index,],family = binomial)
@@ -17,3 +15,18 @@ training_index<-sample(1:nrow(cancer_data_new),0.8*nrow(cancer_data_new))
 library(foreign)
 library(MASS) 
 m<-polr(Level~.,data = cancer_data_new[training_index,])
+
+#Multinomial logistic regression
+require(foreign)
+require(nnet)
+require(ggplot2)
+require(reshape2)
+
+cancer_data_new$Level<-relevel(cancer_data_new$Level,ref = "Low")
+ml<-multinom(Level~.,data = cancer_data_new,subset = training_index)
+summary(ml)
+ml.fit<-predict(ml,cancer_data_new[-training_index,])
+mean((ml.fit!=apply(cancer_data_new[-training_index,],2,factor))^2)
+
+
+
