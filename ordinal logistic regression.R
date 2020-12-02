cf4898e5dec02893fcaa55ac59317b76e45cd80b
建颖 梁 <UnaLiang@jianyings-MacBook-Pro.local>
2020-12-02 23:29:29 +0800

    100% accuracy wtf lol

--- ordinal logistic regression.R
+++ ordinal logistic regression.R
@@ -28,5 +28,26 @@ summary(ml)
 ml.fit<-predict(ml,cancer_data_new[-training_index,])
 mean((ml.fit!=apply(cancer_data_new[-training_index,],2,factor))^2)
 
+library(leaps)
+sum(is.na(cancer_data_new))
+cancer_data_new<-na.omit(cancer_data_new)
+regfit.full=regsubsets(Level~.,cancer_data_new) #all subset selection 
+summary(regfit.full)
+
+cancer_data_train<-cancer_data_new[training_index,]
+cancer_data_test<-cancer_data_new[-training_index,]
+x=model.matrix(Level~.,cancer_data_train)[,-1]
+new_x<-model.matrix(Level~.,cancer_data_test)[,-1]
+y<-cancer_data_train$Level
+library(glmnet)
+cvfit = cv.glmnet(x,y,type.measure="class",alpha=1,family="multinomial")
+coef(cvfit, s = "lambda.min")
+bestlam=cvfit$lambda.min
+lasso.mod=glmnet(x,y,alpha=1,lambda=bestlam,family="multinomial")
+pred.lasso = predict(lasso.mod, s = bestlam, newx = new_x,type = "response")
+predicted <- colnames(pred.lasso)[apply(pred.lasso,1,which.max)]
+
+mean(predicted!=cancer_data_test$Level)
+
 
 
